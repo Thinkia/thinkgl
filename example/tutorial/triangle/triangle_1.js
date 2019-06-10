f6c3134719b22764d4439150193086e523a54423
Thinkia <1270301161@qq.com>
2019-06-10 15:02:48 +0800

    add  triangle1 demo

---
+++ example/tutorial/triangle/triangle_1.js
@@ -0,0 +1,44 @@
+/**
+ * 2019.6.10
+ *
+ * 这里演示用 连线的方式画一个三角形
+ *
+ */
+
+
+let ia = Ia();
+
+let iaWorld = ia.world;
+
+ia.action.view.jump([0,0,-5])
+
+let positions = [
+
+    -1.0,0.0,
+    4.0,0.0,
+    1.5,2.0,
+
+];
+
+let colors = [
+
+    1.0,0.0,0.0,1.0,   // 红 red
+    1.0,0.0,0.0,1.0,   // 红 red
+    1.0,0.0,0.0,1.0,   // 红 red
+
+]
+
+main();
+
+function main() {
+
+    iaWorld.initIaWorld( );
+
+    let buffers = iaWorld.buffer.positionBuffer.initBuffer( positions ,colors );
+
+    iaWorld.helloIaWorld( buffers );
+
+    iaWorld.drawLines(3);
+
+
+}
