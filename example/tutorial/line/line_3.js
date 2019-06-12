fba7f99abb6ee58944dd5e10b7bd4ed2b469428f
Thinkia <1270301161@qq.com>
2019-06-12 13:38:24 +0800

    add tutorial line_4     rotate animate

---
+++ example/tutorial/line/line_3.js
@@ -0,0 +1,52 @@
+/**
+ * 2019.6.12
+ *
+ * 这里演示  线的基本旋转动画
+ *
+ */
+
+let ia = Ia();
+
+let iaWorld = ia.world;
+
+ia.action.view.jump([0,0,-5])
+
+let positions = [
+    0.0,0.0,
+    3.0, 0.0,
+
+];
+
+let colors = [
+
+    1.0,0.0,0.0,1.0,   //  red
+    1.0,0.0,0.0,1.0,   //  red
+
+
+];
+
+main();
+
+function main() {
+
+    iaWorld.initIaWorld( );
+
+    let buffers = iaWorld.buffer.positionBuffer.initBuffer( positions ,colors );
+
+    render();
+
+    function render() {
+
+        // 绕向量[0,0,-1] 每帧旋转 2度     60f/s
+
+        ia.action.view.rotate([0,0,-1],0.5*Math.PI/180);
+
+        iaWorld.helloIaWorld( buffers );
+        iaWorld.drawLines();
+
+        requestAnimationFrame(render)
+
+    }
+
+
+}
